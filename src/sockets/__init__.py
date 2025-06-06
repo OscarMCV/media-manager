841789afe44300ad8c89ecf5a4a786f8c5e9e959
OscarMCV <oscarm.cabrerav@gmail.com>
2025-06-06 08:08:40 -0600

    add socket module initialization and import utilities for event loop management

---
+++ src/sockets/__init__.py
@@ -0,0 +1,9 @@
+from .process_broadcaster.utils import (
+    get_event_loop,
+    broadcaster_handler_context_manager,
+)
+
+__all__ = [
+    "get_event_loop",
+    "broadcaster_handler_context_manager",
+]
