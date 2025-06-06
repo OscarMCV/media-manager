eb471c36424c3ee14cc544f6445cb858b67bd3ce
OscarMCV <oscarm.cabrerav@gmail.com>
2025-06-06 08:15:28 -0600

    add ProcessBroadcaster to exports in sockets module

--- src/sockets/__init__.py
+++ src/sockets/__init__.py
@@ -2,8 +2,10 @@ from .process_broadcaster.utils import (
     get_event_loop,
     broadcaster_handler_context_manager,
 )
+from .process_broadcaster.process_broadcaster import ProcessBroadcaster
 
 __all__ = [
     "get_event_loop",
     "broadcaster_handler_context_manager",
+    "ProcessBroadcaster",
 ]
